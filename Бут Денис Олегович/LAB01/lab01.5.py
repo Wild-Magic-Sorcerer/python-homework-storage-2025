09376c46598bc0bd9cb7157013cd2d69582f9b9b
DeniroNl <denibutfuture@gmail.com>
2025-10-03 10:57:12 +0300

    Delete Бут Денис Олегович/LAB01/lab01.5.py

--- Бут Денис Олегович/LAB01/lab01.5.py
+++
@@ -1,4 +0,0 @@
-if __name__ == "__main__":
-    main_list = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    new_list = [x for x in a if x % 3 == 0]
-    print(new_list)
