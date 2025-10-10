a519c0be6a106afa22c3d5b961c02b40f6df2e17
Marina-Alp <marzepan20@gmail.com>
2025-10-10 11:03:25 +0300

    Доработала

--- Алпацкая Марина Фёдоровна/LAB01/task02.py
+++ Алпацкая Марина Фёдоровна/LAB01/task02.py
@@ -1,27 +1,29 @@
 def number_chek(list_chekc: dict[str: dict[str: int]]):
     for i in list_chekc:
-        print(f"\nСредняя оценка по предмету '{i}': {sum(int(list_chekc[i][num]) for num in list_chekc[i])/len(list_chekc[i])}")
-        print(f"Максимальная оценка по предмету '{i}': {max(int(list_chekc[i][num]) for num in list_chekc[i])}")
-        print(f"Минимальная оценка по предмету '{i}': {min(int(list_chekc[i][num]) for num in list_chekc[i])}")
+        print(f"\nСредняя оценка по предмету '{i}': {sum(list_chekc[i][num] for num in list_chekc[i])/len(list_chekc[i])}")
+        print(f"Максимальная оценка по предмету '{i}': {max(list_chekc[i][num] for num in list_chekc[i])}")
+        print(f"Минимальная оценка по предмету '{i}': {min(list_chekc[i][num] for num in list_chekc[i])}")
     return None
 
 if __name__ == '__main__':
     students: dict[str: dict[str: int]]= {"програмирование пайтон": {}, "цифравая аналитика": {}, "введение в биоинформатику": {}}
-    print('Ведите имина студентов\n Когда закончите вводить студентов введите букву: "N"')
-    name = []
-    while 'N' and 'n' not in name:
-        name.append(input('Введите имя студента: '))
-    name.pop()
+    print('Ведите имина студентов\nКогда закончите вводить студентов введите букву: "N"')
+    names = []
+    while True:
+        name = input('Введите имя студента: ')
+        if 'n' == name.lower() or '' == name:
+            break
+        names.append(name)
 
     for journal in students:
-        for num in name:
-            t_f = True
-            while t_f == True:
-                students[journal][num] = input(f'Введите оценку по предмету "{journal}" которую получил/ла {num}: ')
-                if students[journal][num].isdigit():
-                    if 2<= int(students[journal][num]) <= 5:
-                        t_f = False
-                        continue
+        for nam in names:
+            while True:
+                temp = input(f'Введите оценку по предмету "{journal}" которую получил/ла {nam}: ')
+                if temp.isdigit():
+                    temp_int = int(temp)
+                    if 2<= temp_int <= 5:
+                        students[journal][nam] = temp_int
+                        break
                 print("Вы ввели не валидное значение, пожалуйста попробуйте снова")
 
     number_chek(students)
