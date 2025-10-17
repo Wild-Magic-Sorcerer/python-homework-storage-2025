4a17d8028ddb4e9e38d788ae0c97f3399901d266
Igor Tsiplakov <igorcyplakov4@gmail.com>
2025-10-17 21:49:18 +0300

    Update lab05_05.py
    Добавлено задание

--- lab05/lab05_05.py
+++ lab05/lab05_05.py
@@ -1,3 +1,12 @@
 # Реализуйте простую систему ведения лога: создайте функцию с именем logger
 # которая принимает текст сообщения от пользователя и время (по умолчанию — текущее)
 # и записывает лог в отдельный файл (log.txt), дописывая в конец файла каждое новое сообщение
+
+from datetime import datetime as dt
+def loger(user_str):
+    with open('log.txt', 'a') as log:
+        log.write(f'{user_str} | {dt.now()}\n')
+print('Введи-те свой текст')
+user = str(input())
+print('Ваш текст сохранён в файл: log.txt')
+loger(user)
