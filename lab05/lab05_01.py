4328f1459da15f2759a4e302eb7d0b57d5b99712
Igor Tsiplakov <igorcyplakov4@gmail.com>
2025-10-17 21:45:06 +0300

    Update lab05_01.py
    Исправлены некоторые ошибки и переписана часть кода

--- lab05/lab05_01.py
+++ lab05/lab05_01.py
@@ -1,24 +1,15 @@
 # Создайте файл "numbers.txt" с 10 произвольными числами (каждое на новой строке)
 # Напишите программу, которая читает файл, находит и выводит максимальное, минимальное и среднее из файла.
 
+a = (45, 79, 87, 0, -1, -15, 0, 98, 2, 13)
 with open('numbers.txt', 'w') as file:
-    file.write('1\n')
-    file.write('1\n')
-    file.write('1\n')
-    file.write('1\n')
-    file.write('1\n')
-    file.write('1\n')
-    file.write('1\n')
-    file.write('1\n')
-    file.write('0\n')
-    file.write('2\n')
-    file.close()
+    for i in a:
+        file.write(f'{i}\n')
 file_in_list = []
 with open('numbers.txt', 'r') as file:
-    for i in range(10):
+    for i in a:
         lines_from_file = int(file.readline().replace('\n', ''))
         file_in_list.append(lines_from_file)
     print('Максимальное число в файле:', max(file_in_list))
     print('Минимальное число в файле:', min(file_in_list))
-    print('среднее число в файле:', sum(file_in_list)/10)
-    file.close()
+    print('среднее число в файле:', sum(file_in_list)/len(a))
