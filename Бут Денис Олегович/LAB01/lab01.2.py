9e406d95bafa051233ab81a56aaecbd583c0bf53
DeniroNl <denibutfuture@gmail.com>
2025-11-16 15:53:21 +0300

    Add files via upload

--- Бут Денис Олегович/LAB01/lab01.2.py
+++ Бут Денис Олегович/LAB01/lab01.2.py
@@ -1,3 +1,6 @@
+from tkinter.font import names
+
+
 def calculate_biggest_value(dict_of_values):
     if not dict_of_values:
         return None
@@ -25,39 +28,21 @@ def calculate_smallest_value(dict_of_values):
 
 
 if __name__ == "__main__":
-    course_and_students = {
-        "Higher mathematics": {"Миша": 5, "Савелий": 2, "Маша": 4},
-        "Physics": {"Игорь": 2, "Света": 6, "Любовь": 4},
-        "Zoology": {"Саша": 3, "Игнат": 2, "Мирослав": 5},
-        "Botany": {"Соня": 1, "Инокентий": 4, "Тосиро": 5},
-    }
-    dict_in_dict = input(
-        "Выберите предмет: Higher mathematics, Physics,Zoology,Botany "
+    courses = (
+        "Higher mathematics",
+        "Physics",
+        "Zoology",
+        "Botany",
     )
-    tuple_of_name = course_and_students[dict_in_dict]
-    list_of_name = course_and_students[dict_in_dict].keys()
-    print(list_of_name)
-
-    name_of_student = input("Выберите студента:")
-    value = tuple_of_name[name_of_student]
-    if 3 <= value <= 5:
-        print("good")
-    else:
-        print("wrong value")
-
-    sum_of_value = 0
-    list_of_value = []
-    for i in tuple_of_name:
-        student_value3 = tuple_of_name[i]
-        if student_value3 <= 5:
-            list_of_value.append(student_value3)
-            sum_of_value = student_value3 + sum_of_value
-        else:
-            continue
-        average_value: float = sum_of_value / len(list_of_value)
-
-    print(
-        calculate_biggest_value(tuple_of_name),
-        calculate_smallest_value(tuple_of_name),
-        average_value,
-    )
+    names=[]
+    while True:
+        student_name = input("Введите имя студента. N/n если всё.")
+        if student_name.lower() == "n" or student_name == "" :
+            break
+        names.append(student_name)
+    print(names)
+    result: dict[str: dict[str: int]]= {}
+    for course in courses:
+        value= input(f"Введите оценку студента {student_name} по курсу {courses}")
+        result[student_name][course] = value
+    print(result)
