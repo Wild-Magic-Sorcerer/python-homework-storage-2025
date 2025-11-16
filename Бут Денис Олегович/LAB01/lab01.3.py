b13a7c37f8e7221e96c86447ca14ea8892db3223
DeniroNl <denibutfuture@gmail.com>
2025-11-16 15:52:15 +0300

    Delete Бут Денис Олегович/LAB01/lab01.3.py

--- Бут Денис Олегович/LAB01/lab01.3.py
+++
@@ -1,7 +0,0 @@
-def tuple_and_set(words):
-    split_w = words.split(" ")
-    return print("Кортёж слов:", split_w, "Число уникальных слов:", len(set(split_w)))
-
-if __name__ == "__main__":
-    words = input() 
-    print(tuple_and_set(words))
