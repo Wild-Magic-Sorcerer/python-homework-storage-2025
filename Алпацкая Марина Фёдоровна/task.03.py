74260019df479127171ec70e17e6e7485a840dcf
Marina-Alp <marzepan20@gmail.com>
2025-12-05 10:07:55 +0300

    Delete Алпацкая Марина Фёдоровна/task.03.py

--- Алпацкая Марина Фёдоровна/task.03.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-    copy_fail = ''
-    with open('cat.txt', 'r') as cat:
-        copy_fail = cat.read()
-
-    with open("dog.txt", 'w') as dog:
-        new_dog = copy_fail.replace('cat','dog')
-        dog.write(new_dog)
