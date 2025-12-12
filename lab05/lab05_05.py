522b0ace182d42ea512369da28d54816b8319b79
Igor Tsiplakov <igorcyplakov4@gmail.com>
2025-12-12 10:42:05 +0300

    Update lab05_05.py
    внесены изменения

--- lab05/lab05_05.py
+++ lab05/lab05_05.py
@@ -3,10 +3,10 @@
 # и записывает лог в отдельный файл (log.txt), дописывая в конец файла каждое новое сообщение
 
 from datetime import datetime as dt
+
 def loger(user_str):
     with open('log.txt', 'a') as log:
         log.write(f'{user_str} | {dt.now()}\n')
-print('Введи-те свой текст')
-user = str(input())
-print('Ваш текст сохранён в файл: log.txt')
+user = str(input('Введи-те свой текст '))
 loger(user)
+print('Ваш текст сохранён в файл: log.txt')
