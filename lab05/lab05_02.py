c65fbbef599c5020a0063db6b3427fb66443fddd
Igor Tsiplakov <igorcyplakov4@gmail.com>
2025-12-12 10:33:18 +0300

    Update lab05_02.py
    добавлены изменения

--- lab05/lab05_02.py
+++ lab05/lab05_02.py
@@ -1,14 +1,9 @@
 # Реализуйте программу, которая запрашивает у пользователя строку и добавляет её в существующий текстовый файл
 # После добавления выведите все строки этого файла на экран.
 
-print('Пожалуйста введите строчку своего текста')
-line_from_user = str(input())
+line_from_user = input('Пожалуйста введите строчку своего текста ')
 with open('lab05_02_file.txt', 'a') as file:
     file.write(f'{line_from_user}\n')
 with open('lab05_02_file.txt', 'r') as file:
-    while True:
-        line = file.readline()
-        if not line:
-            line = file.readline().replace('\n', '')
-            continue
-        print(line)
+    for line in file.readlines():
+        print(line.replace('\n', ''))
