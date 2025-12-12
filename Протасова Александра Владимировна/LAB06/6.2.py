579de12594c5bc1856d7ec6787f9b2b7be076f83
aleksandra-protasova <sanny_2006@mail.ru>
2025-12-12 11:32:33 +0300

    Add files via upload

--- Протасова Александра Владимировна/LAB06/6.2.py
+++ Протасова Александра Владимировна/LAB06/6.2.py
@@ -9,12 +9,13 @@ class BankAccount:
         """Положить деньги на счет"""
         if amount > 0:
             self.__money += amount
+
     def take_money(self, amount):
         """Снять деньги со счета"""
         if 0 < amount <= self.__money:
             self.__money -= amount
         else:
-           raise ...
+           raise ValueError("Невозможно снять указанную сумму")
 
     def show_money(self):
         """Показать баланс"""
@@ -23,22 +24,15 @@ class BankAccount:
 if __name__ == "__main__":
     my_account = BankAccount(1100)
     sasha_account = BankAccount(800)
+
     print(f"На счету: {my_account.show_money()} руб")
 
-    if my_account.take_money(300):
-        print("Успешно сняли 300 руб")
-    else:
-        print("Не удалось снять 300 руб")
-    print(f"Осталось: {my_account.show_money()} руб")
+    try:
+        my_account.take_money(20000)
+        print('Успех, деньги сняты')
+    except ValueError:
+        print('Провал, мало денег')
 
-    if my_account.take_money(2000):
-        print("Сняли 2000 руб")
-    else:
-        print("Нельзя снять 2000 руб - мало денег")
-
-    if my_account.add_money(500):
-        print("Положили 500 руб")
-    print(f"Итого: {my_account.show_money()} руб")
 
     print(f"У Саши: {sasha_account.show_money()} руб")
     sasha_account.add_money(1000)
