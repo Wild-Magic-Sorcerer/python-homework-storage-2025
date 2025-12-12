96a1851b8d51215836e5b93e267b82d9afe57ea8
Igor Tsiplakov <igorcyplakov4@gmail.com>
2025-12-12 10:32:08 +0300

    Update lab05_01.py
    добавлены изменения

--- lab05/lab05_01.py
+++ lab05/lab05_01.py
@@ -1,15 +1,14 @@
 # Создайте файл "numbers.txt" с 10 произвольными числами (каждое на новой строке)
 # Напишите программу, которая читает файл, находит и выводит максимальное, минимальное и среднее из файла.
 
-a = (45, 79, 87, 0, -1, -15, 0, 98, 2, 13)
+numbers_list = (45, 79, 87, 0, -1, -15, 0, 98, 2, 13)
 with open('numbers.txt', 'w') as file:
-    for i in a:
+    for i in numbers_list:
         file.write(f'{i}\n')
-file_in_list = []
 with open('numbers.txt', 'r') as file:
-    for i in a:
-        lines_from_file = int(file.readline().replace('\n', ''))
-        file_in_list.append(lines_from_file)
+    file_in_list = []
+    lines_from_file = int(file.readline().replace('\n', ''))
+    file_in_list.append(lines_from_file)
     print('Максимальное число в файле:', max(file_in_list))
     print('Минимальное число в файле:', min(file_in_list))
-    print('среднее число в файле:', sum(file_in_list)/len(a))
+    print('среднее число в файле:', sum(file_in_list)/len(numbers_list))
