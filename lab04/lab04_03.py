32571e168de69b38ed1b469a1bfd62b9f4dc9799
Igor Tsiplakov <igorcyplakov4@gmail.com>
2025-12-12 10:27:16 +0300

    Update lab04_03.py
    добавлено главное тело

--- lab04/lab04_03.py
+++ lab04/lab04_03.py
@@ -3,6 +3,7 @@
 
 from functools import reduce as rd
 
-our_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-l = lambda x, y: x + y
-print(rd(l, our_list))
+if __name__ == '__main__':
+    our_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    l = lambda x, y: x + y
+    print(rd(l, our_list))
