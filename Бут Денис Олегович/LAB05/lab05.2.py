9eb5b84e7a2b434784e2381c7a0f98a2783f0d50
DeniroNl <denibutfuture@gmail.com>
2025-12-15 22:33:38 +0300

    Update lab05.2.py

--- Бут Денис Олегович/LAB05/lab05.2.py
+++ Бут Денис Олегович/LAB05/lab05.2.py
@@ -1,8 +1,9 @@
 if __name__ == '__main__':
     users_str = input("Введите строку")
     with open("file_for_lab05.2.txt", "a") as file:
-        file.write(f"\n{users_str}")
+        file.write(f"{users_str}\n")
     with open("file_for_lab05.2.txt", "r") as file:
         lines = file.read()
         print(f"Содержимое файла:\n {lines}")
+
 
