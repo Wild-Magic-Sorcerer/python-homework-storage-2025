a847903739ea851d902a41ae46b65734d24e5120
DeniroNl <denibutfuture@gmail.com>
2025-12-15 22:32:48 +0300

    Update lab05.1.py

--- Бут Денис Олегович/LAB05/lab05.1.py
+++ Бут Денис Олегович/LAB05/lab05.1.py
@@ -3,8 +3,8 @@ def calculate_big_one(def_for_big_num):
         return None
     biggest_num = int(def_for_big_num[0])
     for num in def_for_big_num:
-        if int(num) > biggest_num:
-            biggest_num= int(num)
+        if num > biggest_num:
+            biggest_num= num
     return biggest_num
 
 
@@ -13,8 +13,8 @@ def calculate_little_one(def_for_smallest_num):
         return None
     smallest_num = int(def_for_smallest_num[0])
     for num in def_for_smallest_num:
-        if int(num) < smallest_num:
-            smallest_num = int(num)
+        if num < smallest_num:
+            smallest_num = num
     return smallest_num
 
 def average(some_list):
@@ -26,12 +26,16 @@ def average(some_list):
 
 
 if __name__ == "__main__":
-    list_of_sum = [0]
     list_of_numbs = []
     with open("numbers_for_lab05.1.txt", "r") as file_numbers:
         for line in file_numbers:
-            list_of_numbs.append(float(line))
+             try:
+                value = int(line)
+                list_of_numbs.append(value)
+            except ValueError:
+                print("Value is not an integer")
         print(list_of_numbs)
         print( "Наибольшее число =",calculate_big_one(list_of_numbs),"\n"
                "Наименьшее число =",calculate_little_one(list_of_numbs),"\n"
                "Среднее чисел =",average(list_of_numbs))
+
