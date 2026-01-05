46dbf971681247f3931a12bca2c4fefa8ff50b0c
Marina-Alp <marzepan20@gmail.com>
2026-01-05 21:44:42 +0300

    Алпацкая Марина Lab7-8

---
+++ Алпацкая Марина Фёдоровна/LAB07/tasck.03.py
@@ -0,0 +1,12 @@
+import argparse
+if __name__ == '__main__':
+    lines = argparse.ArgumentParser(description='Строки чисил')
+    lines.add_argument('line', nargs='+')
+    lines.add_argument('-f','--flag', type=bool, default=False)
+    list_strok = lines.parse_args()
+
+    if list_strok.flag:
+        print(len(list_strok.line))
+    else:
+        for i in list_strok.line:
+            print(i)
