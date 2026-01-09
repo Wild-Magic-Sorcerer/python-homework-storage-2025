4aa85b424f7a4b5ddbb9a228c329330423ce5e54
LisicDisma <kotukkc@gmail.com>
2026-01-09 17:32:52 +0300

    Update task03.py

--- Шабанова Ксения Алексеевна/LAB07/task03.py
+++ Шабанова Ксения Алексеевна/LAB07/task03.py
@@ -1,13 +1,19 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("strings", nargs='+', help="Список строк")
-parser.add_argument("--count", action="store_true", help="Посчитать количество")
-
-args = parser.parse_args()
-
-if args.count:
-    print(f"Количество строк: {len(args.strings)}")
-else:
-    for s in args.strings:
-        print(s)
+def main():
+    import argparse
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("strings", nargs='+', help="Список строк")
+    parser.add_argument("--count", action="store_true", help="Посчитать количество")
+    
+    args = parser.parse_args()
+    
+    if args.count:
+        print(f"Количество строк: {len(args.strings)}")
+    else:
+        for s in args.strings:
+    
+            print(s)
+            
+if __name__ == "__main__":
+    main()
+    
