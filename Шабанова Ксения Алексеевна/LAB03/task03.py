ce924495eb7e3d392cf7e988897862339fefa75c
LisicDisma <kotukkc@gmail.com>
2026-01-09 17:18:09 +0300

    Update task03.py

--- Шабанова Ксения Алексеевна/LAB03/task03.py
+++ Шабанова Ксения Алексеевна/LAB03/task03.py
@@ -1,14 +1,19 @@
-def multiply_ints(*args):
-    integers = [x for x in args if isinstance(x, int)]
+def main():
+    def multiply_ints(*args):
+        integers = [x for x in args if isinstance(x, int)]
+        
+        if not integers:
+            return None
+        
+        res = 1
+        for n in integers:
+            res *= n
+        return res
     
-    if not integers:
-        return None
+    # Проверяем
+    print(f"Результат с числами: {multiply_ints(2, 'текст', 3, 4.5, 10)}")
     
-    res = 1
-    for n in integers:
-        res *= n
-    return res
+    print(f"Результат без целых: {multiply_ints('привет', 0.5)}")
 
-# Проверяем
-print(f"Результат с числами: {multiply_ints(2, 'текст', 3, 4.5, 10)}")
-print(f"Результат без целых: {multiply_ints('привет', 0.5)}")
+if __name__ == "__main__":
+    main()
