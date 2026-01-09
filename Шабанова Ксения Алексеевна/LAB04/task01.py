1a347fd23cbcee6d8a0256f9efe0c9d0a550942d
LisicDisma <kotukkc@gmail.com>
2026-01-09 17:21:01 +0300

    Update task01.py

--- Шабанова Ксения Алексеевна/LAB04/task01.py
+++ Шабанова Ксения Алексеевна/LAB04/task01.py
@@ -1,6 +1,13 @@
-# Создаем лямбду
-is_even = lambda x: x % 2 == 0
+def main():
+    # Создаем лямбду
+    is_even = lambda x: x % 2 == 0
+    
+    # Проверяем в диапазоне от 1 до 10
+    for i in range(1, 11):
+    
+        print(f"Число {i}: {is_even(i)}")
+        
+if __name__ == "__main__":
+    main()
 
-# Проверяем в диапазоне от 1 до 10
-for i in range(1, 11):
-    print(f"Число {i}: {is_even(i)}")
+
