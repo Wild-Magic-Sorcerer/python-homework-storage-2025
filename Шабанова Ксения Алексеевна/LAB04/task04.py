aee9f4cce21ffc1f0faf687c52d20f70bb17c81b
LisicDisma <kotukkc@gmail.com>
2026-01-09 17:22:50 +0300

    Update task04.py

--- Шабанова Ксения Алексеевна/LAB04/task04.py
+++ Шабанова Ксения Алексеевна/LAB04/task04.py
@@ -1,10 +1,17 @@
-import random
+def main():
+  import random
+  
+  # Создаем произвольный список (я взяла random для интереса)
+  my_list = [random.randint(-20, 20) for _ in range(15)]
+  
+  # Фильтруем
+  filtered_list = list(filter(lambda x: x > -5 and x % 2 == 0, my_list))
+  
+  print(f"Исходный список: {my_list}")
+  
+  print(f"Отфильтрованный список: {filtered_list}")
 
-# Создаем произвольный список (я взяла random для интереса)
-my_list = [random.randint(-20, 20) for _ in range(15)]
+if __name__ == "__main__":
+    main()
 
-# Фильтруем
-filtered_list = list(filter(lambda x: x > -5 and x % 2 == 0, my_list))
 
-print(f"Исходный список: {my_list}")
-print(f"Отфильтрованный список: {filtered_list}")
