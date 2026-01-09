27e5c073b16d0e37b76f0a6051d57d2aa05b9787
LisicDisma <kotukkc@gmail.com>
2026-01-09 17:33:25 +0300

    Update task04.py

--- Шабанова Ксения Алексеевна/LAB07/task04.py
+++ Шабанова Ксения Алексеевна/LAB07/task04.py
@@ -1,18 +1,24 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--input", required=True, help="Входной файл")
-parser.add_argument("--output", required=True, help="Выходной файл")
-
-args = parser.parse_args()
-
-try:
-    with open(args.input, 'r', encoding='utf-8') as f_in:
-        content = f_in.read()
+def main():
+    import argparse
     
-    with open(args.output, 'w', encoding='utf-8') as f_out:
-        f_out.write(content)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", required=True, help="Входной файл")
+    parser.add_argument("--output", required=True, help="Выходной файл")
+    
+    args = parser.parse_args()
+    
+    try:
+        with open(args.input, 'r', encoding='utf-8') as f_in:
+            content = f_in.read()
         
-    print(f"Успешно! Файл '{args.input}' скопирован в '{args.output}'")
-except FileNotFoundError:
-    print("Ошибка: Входной файл не найден")
+        with open(args.output, 'w', encoding='utf-8') as f_out:
+            f_out.write(content)
+            
+        print(f"Успешно! Файл '{args.input}' скопирован в '{args.output}'")
+    except FileNotFoundError:
+    
+        print("Ошибка: Входной файл не найден")
+        
+if __name__ == "__main__":
+    main()
+
