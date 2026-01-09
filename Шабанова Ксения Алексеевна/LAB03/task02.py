aaebd7dfc6f3c936447ae7a5790da1dbefc2cbf1
LisicDisma <kotukkc@gmail.com>
2026-01-09 17:14:54 +0300

    Update task02.py

--- Шабанова Ксения Алексеевна/LAB03/task02.py
+++ Шабанова Ксения Алексеевна/LAB03/task02.py
@@ -1,13 +1,19 @@
-def get_sums(numbers):
-    even_sum = sum(n for n in numbers if n % 2 == 0)
-    odd_sum = sum(n for n in numbers if n % 2 != 0)
-    return odd_sum, even_sum
+def main():
+    def get_sums(numbers):
+        even_sum = sum(n for n in numbers if n % 2 == 0)
+        odd_sum = sum(n for n in numbers if n % 2 != 0)
+        return odd_sum, even_sum
+    
+    # Основная программа
+    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    sum_o, sum_e = get_sums(nums)
+    
+    print("--- Результаты расчёта ---")
+    print(f"Сумма нечётных чисел: {sum_o}")
+    print(f"Сумма чётных чисел:   {sum_e}")
+    
+    print("-" * 26)
 
-# Основная программа
-nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-sum_o, sum_e = get_sums(nums)
+if __name__ == "__main__":
+    main()
 
-print("--- Результаты расчёта ---")
-print(f"Сумма нечётных чисел: {sum_o}")
-print(f"Сумма чётных чисел:   {sum_e}")
-print("-" * 26)
