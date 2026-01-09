fb073d7c3bf83a14814c98e286d63c314785696e
LisicDisma <kotukkc@gmail.com>
2026-01-09 22:54:09 +0300

    Update task02.py

--- Шабанова Ксения Алексеевна/LAB02/task02.py
+++ Шабанова Ксения Алексеевна/LAB02/task02.py
@@ -1,28 +1,28 @@
-def main():
-    def analyze_string(s):
-        s = s.strip()
-                words_list = s.split()
-        
-        stats = {
-            "words": len(words_list),
-            "letters": 0,
-            "digits": 0,
-            "spaces": 0,
-            "punctuation": 0
-        }
-        
-        for char in s:
-            if char.isalpha():      # Проверка на букву
-                stats["letters"] += 1
-            elif char.isdigit():    # Проверка на цифру
-                stats["digits"] += 1
-            elif char.isspace():    # Проверка на пробел
-                stats["spaces"] += 1
-            elif not char.isalnum(): # Если не буква и не цифра (и не пробел, т.к. проверили выше)
-                stats["punctuation"] += 1
-                
-        return stats
+def analyze_string(s):
+    s = s.strip()
+            words_list = s.split()
     
+    stats = {
+        "words": len(words_list),
+        "letters": 0,
+        "digits": 0,
+        "spaces": 0,
+        "punctuation": 0
+    }
+    
+    for char in s:
+        if char.isalpha():      # Проверка на букву
+            stats["letters"] += 1
+        elif char.isdigit():    # Проверка на цифру
+            stats["digits"] += 1
+        elif char.isspace():    # Проверка на пробел
+            stats["spaces"] += 1
+        elif not char.isalnum(): # Если не буква и не цифра (и не пробел, т.к. проверили выше)
+            stats["punctuation"] += 1
+            
+    return stats
+
+def main():   
     # Проверка
     input_text = input("Введите строку для анализа: ")
     result = analyze_string(input_text)
@@ -38,6 +38,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
