6786b1afc6ade36b140e2b26db94a86eee1cf3c3
LisicDisma <kotukkc@gmail.com>
2026-01-09 17:30:23 +0300

    Update task03.py

--- Шабанова Ксения Алексеевна/LAB06/task03.py
+++ Шабанова Ксения Алексеевна/LAB06/task03.py
@@ -1,35 +1,41 @@
-class Person:
-    def __init__(self, name, profession):
-        self.name = name
-        self.profession = profession
+def main():
+    class Person:
+        def __init__(self, name, profession):
+            self.name = name
+            self.profession = profession
+    
+        def introduce(self):
+            print(f"Привет! Я {self.name}, моя профессия — {self.profession}.")
+    
+    class Employee(Person):
+        def __init__(self, name, profession, position):
+            super().__init__(name, profession)
+            self.position = position
+    
+        def introduce(self):
+            # Используем super(), чтобы не дублировать код
+            super().introduce()
+            print(f"На работе я занимаю должность: {self.position}.")
+    
+    class Manager(Employee):
+        def hold_meeting(self):
+            print(f"Менеджер {self.name} собирает совещание!")
+    
+    # Демонстрация
+    p = Person("Анна", "Дизайнер")
+    e = Employee("Игорь", "Программист", "Middle Developer")
+    m = Manager("Елена", "Управленец", "Project Manager")
+    
+    print("--- Полиморфизм и цепочка наследования ---")
+    # Одинаковый вызов для разных классов
+    people = [p, e, m]
+    for person in people:
+        person.introduce()
+        print("-" * 10)
+    
+    
+    m.hold_meeting()
 
-    def introduce(self):
-        print(f"Привет! Я {self.name}, моя профессия — {self.profession}.")
+if __name__ == "__main__":
+    main()
 
-class Employee(Person):
-    def __init__(self, name, profession, position):
-        super().__init__(name, profession)
-        self.position = position
-
-    def introduce(self):
-        # Используем super(), чтобы не дублировать код
-        super().introduce()
-        print(f"На работе я занимаю должность: {self.position}.")
-
-class Manager(Employee):
-    def hold_meeting(self):
-        print(f"Менеджер {self.name} собирает совещание!")
-
-# Демонстрация
-p = Person("Анна", "Дизайнер")
-e = Employee("Игорь", "Программист", "Middle Developer")
-m = Manager("Елена", "Управленец", "Project Manager")
-
-print("--- Полиморфизм и цепочка наследования ---")
-# Одинаковый вызов для разных классов
-people = [p, e, m]
-for person in people:
-    person.introduce()
-    print("-" * 10)
-
-m.hold_meeting()
