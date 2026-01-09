f6369b12e603ef130deeaa6c5a4a3c060334ea0f
LisicDisma <kotukkc@gmail.com>
2026-01-09 15:56:56 +0300

    Update task01.py

--- Шабанова Ксения Алексеевна/LAB01/task01.py
+++ Шабанова Ксения Алексеевна/LAB01/task01.py
@@ -1,16 +1,20 @@
-numbers = []
-print("Введите 10 целых чисел:")
+def main():
+    numbers = []
+    print("Введите 10 целых чисел:")
 
-while len(numbers) < 10:
-    user_input = input(f"Введите число №{len(numbers) + 1}: ")
-    # Проверяем, число ли это 
-    if user_input.lstrip('-').isdigit():
-        num = int(user_input)
-        numbers.append(num)
-    else:
-        print("Это не целое число. Попробуйте еще раз!")
+    while len(numbers) < 10:
+        user_input = input(f"Введите число №{len(numbers) + 1}: ")
+        
+        if user_input.lstrip('-').isdigit() and user_input.lstrip('-') != "":
+            num = int(user_input)
+            numbers.append(num)
+        else:
+            print("Это не целое число. Попробуйте еще раз!")
 
-print("\nРезультаты:")
-print(f"Минимальное число: {min(numbers)}")
-print(f"Максимальное число: {max(numbers)}")
-print(f"Сумма всех чисел: {sum(numbers)}")
+    print("\nРезультаты:")
+    print(f"Минимальное число: {min(numbers)}")
+    print(f"Максимальное число: {max(numbers)}")
+    print(f"Сумма всех чисел: {sum(numbers)}")
+
+if __name__ == "__main__":
+    main()
