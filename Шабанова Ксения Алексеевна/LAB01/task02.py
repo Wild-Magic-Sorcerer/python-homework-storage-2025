6616df4c3610c9eb2876b3c48d787eedb0ba2030
LisicDisma <kotukkc@gmail.com>
2026-01-09 17:59:32 +0300

    Update task02.py

--- Шабанова Ксения Алексеевна/LAB01/task02.py
+++ Шабанова Ксения Алексеевна/LAB01/task02.py
@@ -1,4 +1,23 @@
 def main():
+    def m_max(lst):
+        max_num = 0
+        for el in lst:
+            if el > max_num:
+                max_num = el
+        return max_num
+
+    def m_min(lst):
+        min_num = lst[0]
+        for el in lst:
+            if el < min_num:
+                min_num = el
+        return min_num
+        
+    def m_sum(lst):
+        res = 0
+        for el in lst:
+            res += el
+        return res
     courses = ["Высшая математика", "Программирование python", "Зоология"]
     
     count_students = int(input("Введите количество студентов для оценки: "))
@@ -24,18 +43,17 @@ def main():
                 else:
                     print("    Ошибка: Введите целое число!")
     
-    # Вывод результатов
     if all_marks:
-        average = sum(all_marks) / len(all_marks)
+        average = m_sum(all_marks) / len(all_marks)
         print("\nИтоговая статистика по группе")
         print(f"Средний балл по всем студентам: {average:.2f}")
-        print(f"Минимальная оценка: {min(all_marks)}")
-        print(f"Максимальная оценка: {max(all_marks)}")
+        print(f"Минимальная оценка: {m_min(all_marks)}")
+        print(f"Максимальная оценка: {m_max(all_marks)}")
     else:
-    
         print("\nОценки не были введены.")
 
 if __name__ == "__main__":
     main()
+
 
 
