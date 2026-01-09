2b2638b2fe7d2ae008a288320a7a44450550b247
LisicDisma <kotukkc@gmail.com>
2026-01-09 22:40:19 +0300

    Update task03.py

--- Шабанова Ксения Алексеевна/LAB06/task03.py
+++ Шабанова Ксения Алексеевна/LAB06/task03.py
@@ -1,27 +1,27 @@
+class Person:
+    def __init__(self, name, profession):
+        self.__name = name
+        self.__profession = profession
+
+    def introduce(self):
+        print(f"Привет! Я {self.__name}, моя профессия — {self.__profession}.")
+
+class Employee(Person):
+    def __init__(self, name, profession, position):
+        super().__init__(name, profession)
+        self.__position = position
+
+    def introduce(self):
+        # Используем super(), чтобы не дублировать код
+        super().introduce()
+        print(f"На работе я занимаю должность: {self.__position}.")
+
+class Manager(Employee):
+    def hold_meeting(self):
+        print(f"Менеджер {self.__name} собирает совещание!")
+
+
 def main():
-    class Person:
-        def __init__(self, name, profession):
-            self.__name = name
-            self.__profession = profession
-    
-        def introduce(self):
-            print(f"Привет! Я {self.__name}, моя профессия — {self.__profession}.")
-    
-    class Employee(Person):
-        def __init__(self, name, profession, position):
-            super().__init__(name, profession)
-            self.__position = position
-    
-        def introduce(self):
-            # Используем super(), чтобы не дублировать код
-            super().introduce()
-            print(f"На работе я занимаю должность: {self.__position}.")
-    
-    class Manager(Employee):
-        def hold_meeting(self):
-            print(f"Менеджер {self.__name} собирает совещание!")
-    
-    # Демонстрация
     p = Person("Анна", "Дизайнер")
     e = Employee("Игорь", "Программист", "Middle Developer")
     m = Manager("Елена", "Управленец", "Project Manager")
@@ -38,5 +38,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
