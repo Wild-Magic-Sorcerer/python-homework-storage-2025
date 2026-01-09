88a118fdef9a90079e8495b46ed50c210eec13a8
LisicDisma <kotukkc@gmail.com>
2026-01-09 17:22:08 +0300

    Update task03.py

--- Шабанова Ксения Алексеевна/LAB04/task03.py
+++ Шабанова Ксения Алексеевна/LAB04/task03.py
@@ -1,9 +1,15 @@
-from functools import reduce
+def main():
+  from functools import reduce
+  
+  numbers = list(range(1, 11))
+  
+  # Перемножаем элементы поочереди
+  product = reduce(lambda x, y: x * y, numbers)
+  
+  print(f"Список: {numbers}")
+  
+  print(f"Произведение всех чисел: {product}")
 
-numbers = list(range(1, 11))
+if __name__ == "__main__":
+    main()
 
-# Перемножаем элементы поочереди
-product = reduce(lambda x, y: x * y, numbers)
-
-print(f"Список: {numbers}")
-print(f"Произведение всех чисел: {product}")
