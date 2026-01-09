0e7b7a9aaf29dcf58b45625cf088a82a2c7f1ee3
LisicDisma <kotukkc@gmail.com>
2026-01-09 17:35:37 +0300

    Update task02.py

--- Шабанова Ксения Алексеевна/LAB08/task02.py
+++ Шабанова Ксения Алексеевна/LAB08/task02.py
@@ -1,11 +1,16 @@
-import re
+def main():
+  import re
+  
+  input_str = input("Введите текст с датами: ")
+  
+  # Ищем шаблон ДД.ММ.ГГГГ 
+  date_pattern = r"\d{2}\.\d{2}\.\d{4}"
+  
+  # Заменяем все вхождения на DD.MM.YYYY
+  result = re.sub(date_pattern, "DD.MM.YYYY", input_str)
+  
+  
+  print("Результат:", result)
 
-input_str = input("Введите текст с датами: ")
-
-# Ищем шаблон ДД.ММ.ГГГГ 
-date_pattern = r"\d{2}\.\d{2}\.\d{4}"
-
-# Заменяем все вхождения на DD.MM.YYYY
-result = re.sub(date_pattern, "DD.MM.YYYY", input_str)
-
-print("Результат:", result)
+if __name__ == "__main__":
+    main()
