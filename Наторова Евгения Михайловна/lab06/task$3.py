8ae8db489fc4ec1698f1a32bbf40a8a6ecca4e33
zena876 <nzena605@gmail.com>
2026-01-10 03:30:08 +0300

    Update task$3.py

--- Наторова Евгения Михайловна/lab06/task$3.py
+++ Наторова Евгения Михайловна/lab06/task$3.py
@@ -19,18 +19,20 @@ class Manager(Employee):
         self.department = department
     def hold_meeting(self):
         print(f"мэнеджер {self.name} проводит совещание  отделе {self.department}")
+        
+if __name__ == "__main__":
+    print("первый пункт")
+    persona5= Person('ася', "бухгалтер")
+    emp1= Employee("вася","охранник", "начальник охраны")
+    man1= Manager('Дуся', "менеджер", "руководитель отдела", "инвестиции")
 
-print("первый пункт")
-persona5= Person('ася', "бухгалтер")
-emp1= Employee("вася","охранник", "начальник охраны")
-man1= Manager('Дуся', "менеджер", "руководитель отдела", "инвестиции")
+    print("второй пункт")
+    persona5.introduce()
+    emp1.introduce()
+    man1.introduce()
+    man1.hold_meeting()
+    print("третий пункт")
+    people = [persona5, emp1, man1]
+    for person in people:
+        person.introduce()
 
-print("второй пункт")
-persona5.introduce()
-emp1.introduce()
-man1.introduce()
-man1.hold_meeting()
-print("третий пункт")
-people = [persona5, emp1, man1]
-for person in people:
-    person.introduce()
