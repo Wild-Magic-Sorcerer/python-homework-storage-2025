99d227dd19503804cf9546017472bff7dee8c895
LisicDisma <kotukkc@gmail.com>
2026-01-10 15:54:20 +0300

    Update task01.py

--- Шабанова Ксения Алексеевна/LAB03/task01.py
+++ Шабанова Ксения Алексеевна/LAB03/task01.py
@@ -1,15 +1,17 @@
-def main():
-    def filter_long_strings(strings):
-        if not strings:
-            return []
-        
-        # Считаем среднюю длину: сумма длин всех строк / количество строк
-        avg_length = sum(len(s) for s in strings) / len(strings)
-        
-        # Собираем в новый список те, что длиннее средней
-        result = [s for s in strings if len(s) > avg_length]
-        return result
+def filter_long_strings(strings):
+    if not strings:
+        return []
     
+    # Считаем среднюю длину: сумма длин всех строк / количество строк
+    avg_length = sum(len(s) for s in strings) / len(strings)
+    
+    # Собираем в новый список те, что длиннее средней
+    result = [s for s in strings if len(s) > avg_length]
+    return result
+    
+
+def main():
+   
     # Тестик
     test_list = ["я", "учу", "вышмат", "в", "политехе"]
     
@@ -17,4 +19,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
