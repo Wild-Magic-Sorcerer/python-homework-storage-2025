cc3437fbb998219faba79b92d8bf9556ebc8367b
LissavetaL8 <elizavetalopatinskaa@gmail.com>
2026-01-12 01:20:43 +0300

    Delete Лопатинская Елизавета Сергеевна/Lab01/Lab01 directory

--- Лопатинская Елизавета Сергеевна/Lab01/Lab01/Task5.py
+++
@@ -1,7 +0,0 @@
-original_list = list(range(-10, 11))
-
-filtered_list = [num for num in original_list if num % 3 == 0]
-
-print(f"Исходный список: {original_list}")
-
-print(f"Числа, кратные 3: {filtered_list}")
