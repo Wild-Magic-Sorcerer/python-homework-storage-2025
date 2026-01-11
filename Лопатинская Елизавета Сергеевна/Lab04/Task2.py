e800f65ac51c81b80aed05810461d09d54e2ff1c
LissavetaL8 <elizavetalopatinskaa@gmail.com>
2026-01-12 02:26:14 +0300

    Update Task2.py

--- Лопатинская Елизавета Сергеевна/Lab04/Task2.py
+++ Лопатинская Елизавета Сергеевна/Lab04/Task2.py
@@ -1,7 +1,12 @@
-words = ["apple", "banana", "cherry"]
+def get_word_lengths(words_list):
+    return list(map(lambda s: len(s), words_list))
 
-lengths = list(map(lambda s: len(s), words))
+def main():
+    words = ["apple", "banana", "cherry"]
+    lengths = get_word_lengths(words)
 
-print(f"Исходный список: {words}")
+    print(f"Исходный список: {words}")
+    print(f"Список длин:    {lengths}")
 
-print(f"Список длин: {lengths}")
+if __name__ == "__main__":
+    main()
