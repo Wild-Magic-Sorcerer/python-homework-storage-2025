bda1104250c5cf5af884e7be59fb6ef7e872dfe0
LissavetaL8 <elizavetalopatinskaa@gmail.com>
2026-01-12 02:58:31 +0300

    Update Task2.py

--- Лопатинская Елизавета Сергеевна/Lab08/Task2.py
+++ Лопатинская Елизавета Сергеевна/Lab08/Task2.py
@@ -1,14 +1,22 @@
-import data
+import re
+
 def mask_dates(text):
     date_pattern = r"\d{2}\.\d{2}\.\d{4}"
     result = re.sub(date_pattern, "DD.MM.YYYY", text)
     return result
 
-# Пример работы
-user_input = input("Введите текст, содержащий даты (например, 01.01.2024): ")
-output = mask_dates(user_input)
+def main():
+    print("--- Программа маскирования дат ---")
+    user_input = input("Введите текст, содержащий даты (например, 01.01.2024): ")
+    
+    if user_input.strip():
+        output = mask_dates(user_input)
+        
+        print("\nРезультат обработки:")
+        print(output)
+    else:
+        print("\nОшибка: Введена пустая строка.")
 
-print("\nРезультат обработки:")
-
-print(output)
-
+if __name__ == "__main__":
+    main()
+    
