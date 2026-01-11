5fe958f5371a76fe1bf302f5144d01fe1f6200bd
LissavetaL8 <elizavetalopatinskaa@gmail.com>
2026-01-12 01:21:21 +0300

    Лопатинская Елизавета Сергеевна

---
+++ Лопатинская Елизавета Сергеевна/Lab01/Task1.py
@@ -0,0 +1,20 @@
+numbers = []
+print("Введите 10 целых чисел:")
+
+while len(numbers) < 10:
+    user_input = input(f"Введите число №{len(numbers) + 1}: ")
+    try:
+        num = int(user_input)
+        numbers.append(num)
+    except ValueError:
+        print("Ошибка: нужно ввести корректное целое число.")
+
+min_val = min(numbers)
+max_val = max(numbers)
+total_sum = sum(numbers)
+
+print("-" * 30)
+print(f"Ваш список: {numbers}")
+print(f"Минимальное число: {min_val}")
+print(f"Максимальное число: {max_val}")
+print(f"Сумма всех чисел: {total_sum}")
