809a49eab9ac4910bf3c20054023f608fd85b1fd
LissavetaL8 <elizavetalopatinskaa@gmail.com>
2026-01-12 02:59:57 +0300

    Update Task3.py

--- Лопатинская Елизавета Сергеевна/Lab08/Task3.py
+++ Лопатинская Елизавета Сергеевна/Lab08/Task3.py
@@ -1,17 +1,24 @@
 import re
+
 def split_text(text):
     pattern = r"[.,!?;:\-\s]+"
     words = re.split(pattern, text)
     words = [word for word in words if word]
     return words
 
+def main():
+    print("--- Программа разделения текста на слова ---")
+    user_input = input("Введите предложение со знаками препинания: ")
+    if user_input.strip():
+        result = split_text(user_input)
 
-# Пример работы
-user_input = input("Введите предложение со знаками препинания: ")
-result = split_text(user_input)
+        # Вывод результатов
+        print("\nСписок слов:")
+        print(result)
+        print(f"Всего слов: {len(result)}")
+    else:
+        print("\nОшибка: Строка не должна быть пустой.")
 
-print("\nСписок слов:")
-print(result)
-
-print(f"Всего слов: {len(result)}")
-
+if __name__ == "__main__":
+    main()
+    
