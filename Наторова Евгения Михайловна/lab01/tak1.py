903aba99c6ac0beef83555927f607480a5c3706f
zena876 <nzena605@gmail.com>
2026-01-12 00:47:21 +0300

    Add files via upload

---
+++ Наторова Евгения Михайловна/lab01/tak1.py
@@ -0,0 +1,28 @@
+if __name__ == "__main__":
+    numbers = []
+    for i in range(10):
+        while True:
+            user_input = input(f"введите целое число ")
+            num = int(user_input)
+            numbers.append(num)
+            break
+    
+    if numbers:
+        min_num = numbers[0]
+        for num in numbers:
+            if num < min_num:
+                min_num = num
+        print(f"минимальное число: {min_num}")
+
+        max_num = numbers[0]
+        for num in numbers:
+            if num > max_num:
+                max_num = num
+        print(f"максимальное число: {max_num}")
+
+        total = 0
+        for num in numbers:
+            total += num
+        print(f"сумма всех чисел: {total}")
+    else:
+        print("пусто")
