38a31e9b55b07854cf52807d8bb8cb65fd19152e
LissavetaL8 <elizavetalopatinskaa@gmail.com>
2026-01-11 17:05:32 +0300

    Delete Лопатинская Елизавета Сергеевна/Lab01/Task3.py

--- Лопатинская Елизавета Сергеевна/Lab01/Task3.py
+++
@@ -1,7 +0,0 @@
-input_string = input("Введите слова через пробел: ")
-
-words_tuple = tuple(input_string.split())
-unique_words = set(words_tuple)
-
-print(f"Ваш список слов: {words_tuple}")
-print(f"Количество уникальных слов в вашем списке: {len(unique_words)}")
