464bc38c6119501ad22735170fd1f57d98b8b3f6
LissavetaL8 <elizavetalopatinskaa@gmail.com>
2026-01-11 17:06:45 +0300

    Delete Lab04 directory

--- Lab04/Task3.py
+++
@@ -1,8 +0,0 @@
-from functools import reduce
-
-numbers = list(range(1, 11))
-
-product = reduce(lambda x, y: x * y, numbers)
-
-print(f"Список чисел: {numbers}")
-print(f"Произведение всех элементов (10!): {product}")
