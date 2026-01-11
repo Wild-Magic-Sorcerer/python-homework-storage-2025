ca9cd9dfde26cb66a85dfc461aaef96e16f9e1b4
LissavetaL8 <elizavetalopatinskaa@gmail.com>
2026-01-12 02:03:33 +0300

    Update Task1.py

--- Лопатинская Елизавета Сергеевна/Lab03/Task1.py
+++ Лопатинская Елизавета Сергеевна/Lab03/Task1.py
@@ -2,18 +2,20 @@ def filter_long_strings(strings_list):
     if not strings_list:
         return []
 
-
     avg_length = sum(len(s) for s in strings_list) / len(strings_list)
     print(f"Средняя длина строки: {avg_length:.2f}")
 
     result = [s for s in strings_list if len(s) > avg_length]
     return result
 
-words = ["яблоко", "дом", "программирование", "код", "питон"]
-filtered = filter_long_strings(words)
+def main():
+    print("--- Программа отбора строк по средней длине ---")    
+    words = ["яблоко", "дом", "программирование", "код", "питон"]
+    print(f"Исходный список: {words}")
+    filtered = filter_long_strings(words)
+    print(f"Строки длиннее средней: {filtered}")
+    print("-" * 50)
 
-print(f"Исходный список: {words}")
-
-print(f"Строки длиннее средней: {filtered}")
-
+if __name__ == "__main__":
+    main()
 
