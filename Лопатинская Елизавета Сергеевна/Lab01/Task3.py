8ba1466e318720cd897b77d87861d49f21fe7a98
LissavetaL8 <elizavetalopatinskaa@gmail.com>
2026-01-12 01:35:45 +0300

    Update Task3.py

--- Лопатинская Елизавета Сергеевна/Lab01/Task3.py
+++ Лопатинская Елизавета Сергеевна/Lab01/Task3.py
@@ -1,7 +1,11 @@
-input_string = input("Введите слова через пробел: ")
+def main():
+    input_string = input("Введите слова через пробел: ")
+    words_tuple = tuple(input_string.split())
+    unique_words = set(words_tuple)
 
-words_tuple = tuple(input_string.split())
-unique_words = set(words_tuple)
+    print(f"Ваш список слов: {words_tuple}")
+    print(f"Количество уникальных слов в вашем списке: {len(unique_words)}")
 
-print(f"Ваш список слов: {words_tuple}")
-print(f"Количество уникальных слов в вашем списке: {len(unique_words)}")
+if __name__ == "__main__":
+    main()
+  
