c184762192a4edc301ad869df6d4b18d7640af00
zena876 <nzena605@gmail.com>
2026-01-11 22:58:29 +0300

    Update t3.py

--- Наторова Евгения Михайловна/lab04/t3.py
+++ Наторова Евгения Михайловна/lab04/t3.py
@@ -1,8 +1,9 @@
-if __name__ == "__main__":
-    from functools import reduce
+from functools import reduce
 
+if __name__ == "__main__":
     nums = list(range(1, 11))
     product = reduce(lambda x, y: x * y, nums)
 
     print(f"произведение всех элементов {nums} = {product}")
     
+
