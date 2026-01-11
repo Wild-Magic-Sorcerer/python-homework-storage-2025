55ebbfcfe5fc9a20ffe9fc8763fa089d35c66324
zena876 <nzena605@gmail.com>
2026-01-11 23:09:19 +0300

    Update ta$k3.py

--- Наторова Евгения Михайловна/lab05/ta$k3.py
+++ Наторова Евгения Михайловна/lab05/ta$k3.py
@@ -5,12 +5,13 @@ if __name__ == "__main__":
     with open("suda.txt", "w") as dest_file:
         dest_file.write(content)
 
-    with open("suda.txt", "r+") as file:
+    with open("suda.txt", "r") as file:
         content = file.read()
         new_content = content.replace("cat", "dog")
-        file.seek(0)
+        
+    with open("suda.txt", "w") as file:
         file.write(new_content)
-        file.truncate()
 
     with open("suda.txt", "r") as file:
         print(file.read())
+
