6dfc4cb5686fcdd1e082b2b20e6c11667ef99f94
zena876 <nzena605@gmail.com>
2026-01-11 18:18:19 +0300

    Update tsk3.py

--- Наторова Евгения Михайловна/lab03/tsk3.py
+++ Наторова Евгения Михайловна/lab03/tsk3.py
@@ -12,4 +12,6 @@ def multiint(*args):
 if __name__ == "__main__":
     result1 = multiint(5, 3, 'gegf', 4.5, [1,2,3])
     result2 = multiint( 'лупа', "чайник", [1, 2, 3])
+
     print(result1, result2)
+    
