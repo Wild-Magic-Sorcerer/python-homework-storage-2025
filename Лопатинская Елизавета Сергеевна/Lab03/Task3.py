fa383939dfeef8a9cea254847526796b24e6247f
LissavetaL8 <elizavetalopatinskaa@gmail.com>
2026-01-11 17:13:27 +0300

    Update Task3.py

--- Лопатинская Елизавета Сергеевна/Lab03/Task3.py
+++ Лопатинская Елизавета Сергеевна/Lab03/Task3.py
@@ -10,4 +10,5 @@ def multiply_integers(args):
 
 print(f"Результат 1: {multiply_integers(2, 'текст', 3, 5.5, 4)}")
 print(f"Результат 2: {multiply_integers('hello', [1, 2], True)}")
+
 print(f"Результат 3: {multiply_integers(10, 0, 5)}")
