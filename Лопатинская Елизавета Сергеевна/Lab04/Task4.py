e91946281f3c2b0d5a27c41a0e5eb7880c67f17e
LissavetaL8 <elizavetalopatinskaa@gmail.com>
2026-01-12 02:28:53 +0300

    Update Task4.py

--- Лопатинская Елизавета Сергеевна/Lab04/Task4.py
+++ Лопатинская Елизавета Сергеевна/Lab04/Task4.py
@@ -1,7 +1,15 @@
 import random
-original_list = random.sample(range(-20, 21), 20)
-filtered_list = list(filter(lambda x: x > -5 and x % 2 == 0, original_list))
 
-print(f"Исходный список: {original_list}")
+def filter_numbers(data):
+    return list(filter(lambda x: x > -5 and x % 2 == 0, data))
 
-print(f"Отфильтрованный список ( > -5 и чётные): {filtered_list}")
+def main():
+    original_list = random.sample(range(-20, 21), 20)
+    filtered_list = filter_numbers(original_list)
+  
+    print(f"Исходный список:\n{original_list}\n")
+    print(f"Отфильтрованный список ( > -5 и чётные):\n{filtered_list}")
+
+if __name__ == "__main__":
+    main()
+  
