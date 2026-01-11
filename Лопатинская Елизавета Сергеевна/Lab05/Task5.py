e637c1fa9e5684b8c2d73dd7ecce9b6434a3c0bd
LissavetaL8 <elizavetalopatinskaa@gmail.com>
2026-01-12 02:48:01 +0300

    Update Task5.py

--- Лопатинская Елизавета Сергеевна/Lab05/Task5.py
+++ Лопатинская Елизавета Сергеевна/Lab05/Task5.py
@@ -3,7 +3,6 @@ from datetime import datetime
 def logger(message, timestamp=None):
     if timestamp is None:
         timestamp = datetime.now()
-
     formatted_time = timestamp.strftime("%d.%m.%Y %H:%M:%S")
 
     log_entry = f"[{formatted_time}] - {message}\n"
@@ -11,9 +10,15 @@ def logger(message, timestamp=None):
     with open("log.txt", "a", encoding="utf-8") as file:
         file.write(log_entry)
 
+def main():
+    print("--- Запуск системы логирования ---")
 
-logger("Запуск системы")
-logger("Пользователь 'admin' вошел в систему")
+    logger("Запуск системы")
+    logger("Пользователь 'admin' вошел в систему")
 
-logger("Ошибка доступа к базе данных", timestamp=datetime(2025, 12, 31, 23, 59))
+    logger("Ошибка доступа к базе данных", timestamp=datetime(2025, 12, 31, 23, 59))
+    print("Логи успешно записаны в файл 'log.txt'.")
 
+if __name__ == "__main__":
+    main()
+    
