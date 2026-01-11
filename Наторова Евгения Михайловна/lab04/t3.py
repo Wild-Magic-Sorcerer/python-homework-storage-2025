c46d91b4f75e500e4e41b01850aaa069432b15f8
zena876 <nzena605@gmail.com>
2026-01-11 16:33:55 +0300

    Add files via upload

---
+++ Наторова Евгения Михайловна/lab04/t3.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    from functools import reduce
+
+    nums = list(range(1, 11))
+    product = reduce(lambda x, y: x * y, nums)
+    print(f"произведение всех элементов {nums} = {product}")
---
+++ Наторова Евгения Михайловна/lab04/t5.py
@@ -0,0 +1,19 @@
+if __name__ == "__main__":
+    import datetime
+
+    def decor(func):
+
+        def wrapper(*args, **kwargs):
+            print("выполняется функция...")
+            result = func(*args, **kwargs)
+            print("функция выполнена.")
+            return result
+
+        return wrapper
+
+    @decor
+    def nowtime():
+        return datetime.datetime.now()
+
+    itime = nowtime()
+    print(f"время {itime}")
