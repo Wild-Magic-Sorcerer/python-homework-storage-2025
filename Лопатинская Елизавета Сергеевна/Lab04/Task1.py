89e320e0c84ce7c102a9d6eab28782f3494c3d9f
LissavetaL8 <elizavetalopatinskaa@gmail.com>
2026-01-13 01:28:20 +0300

    Update Task1.py

--- Лопатинская Елизавета Сергеевна/Lab04/Task1.py
+++ Лопатинская Елизавета Сергеевна/Lab04/Task1.py
@@ -1,16 +1,14 @@
-def check_parity(x):
-    return x % 2 == 0
-
 def main():
-    print(f"{'Число':<10} | {'Чётное?':<10}")
-    print("-" * 25)
+    check_parity = lambda x: x % 2 == 0
+    print(f"{'Число':<10} | {'Чётное?':<10} | {'Результат лямбды':<15}")
+    print("-" * 45)
 
     for i in range(1, 11):
         is_even = check_parity(i)
         status = "Да" if is_even else "Нет"
-        print(f"{i:<10} | {status:<10}")
+   
+        print(f"{i:<10} | {status:<10} | {str(is_even):<15}")
 
 if __name__ == "__main__":
     main()
     
-
