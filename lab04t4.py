1f80d878efb79d776416477058eab6ebf0433d86
whydejection <lysenko.gv@edu.spbstu.ru>
2026-01-13 18:11:04 +0300

    Delete lab04t4.py

--- lab04t4.py
+++
@@ -1,6 +0,0 @@
-import random
-random_list = random.sample(range(-20, 21), 20)
-filtered_list = list(filter(lambda x: x > -5 and x % 2 == 0, random_list))
-
-print("Исходный список:", random_list)
-print("Отфильтрованный список:", filtered_list)
