6343940156419a8d2120a5412c13c829bf364594
whydejection <lysenko.gv@edu.spbstu.ru>
2026-01-13 18:02:37 +0300

    Delete Наторова Евгения Михайловна directory

--- Наторова Евгения Михайловна/Lab08/task3.py
+++
@@ -1,8 +0,0 @@
-import re
-
-if __name__== "__main__":
-    text = input("Введите строку для разбития на слова в формат: ")
-    result = re.split(r'[.,!?-]', text)
-    print("Список слов без пустых элементов:", result)
-
-
--- Наторова Евгения Михайловна/lab04/t1.py
+++
@@ -1,7 +0,0 @@
-if __name__ == "__main__":
-    func = lambda x: x % 2 == 0
-
-    for i in range(1, 11):
-
-        print(i, func(i))
-        
