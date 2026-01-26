4c2a8b308f03fff1d63c431c16fc58290dce7b27
DeniroNl <denibutfuture@gmail.com>
2026-01-26 13:31:13 +0300

    Update lab03.5.py

--- Бут Денис Олегович/LAB03/lab03.5.py
+++ Бут Денис Олегович/LAB03/lab03.5.py
@@ -11,12 +11,13 @@ def factorial(number):
 def factorial_rec(number):
     if not isinstance(number,int) or not number:
         return None
-    if number ==0 or number == 1:
+    if number == 0 or number == 1:
         return 1
     return number * factorial_rec(number - 1)
 
 if __name__ == '__main__':
     print(factorial(6))
     print(factorial_rec(5))
+
 
 
