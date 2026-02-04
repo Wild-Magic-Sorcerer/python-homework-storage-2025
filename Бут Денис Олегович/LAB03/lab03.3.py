ce26b815141e8bee090debd92a038e9bd165bee0
DeniroNl <denibutfuture@gmail.com>
2026-02-04 20:00:34 +0300

    Add files via upload

--- Бут Денис Олегович/LAB03/lab03.3.py
+++ Бут Денис Олегович/LAB03/lab03.3.py
@@ -2,12 +2,11 @@ def multiplying_args(*args):
     if not args:
         return None
 
-    numbers = [arg for arg in args if isinstance(arg, int)]
+    numbers = [arg for arg in args if type(arg) == int]
 
-    result = 1
-    intermediate = [result := result * num for num in numbers]
+    intermediate = [num for num in numbers]
 
-    return result, intermediate
+    return intermediate
 if __name__ == "__main__":
     list_for_def = [True,1,"ss",2,(1,4),4]
     print(multiplying_args(*list_for_def))
