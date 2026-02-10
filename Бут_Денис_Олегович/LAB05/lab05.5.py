14c059aa15ced191c4db9b33fa75060f89e58333
DeniroNl <denibutfuture@gmail.com>
2026-02-10 17:58:21 +0300

    Add files via upload

--- Бут_Денис_Олегович/LAB05/lab05.5.py
+++ Бут_Денис_Олегович/LAB05/lab05.5.py
@@ -4,10 +4,20 @@ def logger(input_argument, input_time=None):
     if input_time is None:
         input_time = datetime.datetime.now()
 
-    with open("log_for_lab05.5.txt", "a") as log:
+    with open("log.txt", "a", encoding="utf-8") as log:
         log.write(f"Users data:{input_argument}\n input time: {input_time}\n")
 
 
 if __name__ == '__main__':
-    users_str = input("Введите строку:")
-    logger(users_str)
+    user_input = input("Введите строку: ")
+
+    time_input = input("Время (ГГГГ-ММ-ДД ЧЧ:ММ или Enter для текущего): ")
+
+    if time_input:
+        try:
+            inp_time = datetime.datetime.strptime(time_input, "%Y-%m-%d %H:%M")
+            logger(user_input, inp_time)
+        except ValueError :
+            logger(user_input)
+    else:
+        logger(user_input)
