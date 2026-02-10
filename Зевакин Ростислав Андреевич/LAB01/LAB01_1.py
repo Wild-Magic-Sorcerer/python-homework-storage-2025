f61c53b64e72f43e115afa4bf13400650f12e4e4
fungi-svaga <zevakin.ra@edu.spbstu.ru>
2026-02-10 20:17:44 +0300

    LAB01_1.py
    исправил

--- Зевакин Ростислав Андреевич/LAB01/LAB01_1.py
+++ Зевакин Ростислав Андреевич/LAB01/LAB01_1.py
@@ -1,14 +1,19 @@
-numbers = []
+def main():
+    numbers = []
 
-print('Введите 10 целых чисел:')
+    print('Введите 10 целых чисел:')
 
-for i in range(10):
-    while True:
-        try:
-            num = int(input(f'Число {i +1}: '))
-            numbers.append(num)
-            break
-        except ValueError:
-            print("Введено неверное число")
+    for i in range(10):
+        while True:
+            try:
+                num = int(input(f'Число {i + 1}: '))
+                numbers.append(num)
+                break
+            except ValueError:
+                print("Введено неверное число")
 
-print(f'min: {min(numbers)}, max: {max(numbers)}, sum: {sum(numbers)}')
+    print(f'min: {min(numbers)}, max: {max(numbers)}, sum: {sum(numbers)}')
+
+
+if __name__ == "__main__":
+    main()
