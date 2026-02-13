9797bfe7767a26417ec405c64db44b16cbd2105d
escrzer <101216932+escrzer@users.noreply.github.com>
2026-02-13 18:48:42 +0300

    Переделала в функцию, добавила пустую строку

--- Сутягина Дарья Денисовна/LAB02/task01.py
+++ Сутягина Дарья Денисовна/LAB02/task01.py
@@ -1,15 +1,14 @@
-if __name__ == '__main__':
-    s = input("Введите строку: ")
-
-    palindrome = True
-
+def palindrome(s: str):
     for i in range(len(s) // 2):
         if s[i] != s[len(s) - 1 - i]:
             print("Строка не является палиндромом")
             print(f"Несоответствие начинается с {i} символа")
             print(f"Слева: '{s[i]}', справа: '{s[len(s) - 1 - i]}'")
-            palindrome = False
-            break
+            return
+    print("Строка является палиндромом")
 
-    if palindrome:
-        print("Строка является палиндромом")
+
+if __name__ == "__main__":
+    s = input("Введите строку: ")
+    palindrome(s)
+    
