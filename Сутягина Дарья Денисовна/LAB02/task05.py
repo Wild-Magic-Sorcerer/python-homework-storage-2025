4b9076ce1dda2a58c76dbca23cc8c12f21af3056
escrzer <101216932+escrzer@users.noreply.github.com>
2026-02-13 19:09:45 +0300

    Update task05.py
    Поменяла структуру и добавила вызов функции.

--- Сутягина Дарья Денисовна/LAB02/task05.py
+++ Сутягина Дарья Денисовна/LAB02/task05.py
@@ -1,23 +1,33 @@
 def compare_strings(s1: str, s2: str):
-    punctuation = ".,!?;:-()\"'"
+    punct = ".,!?;:-()\"'"
 
-    for p in punctuation:
-        s1 = s1.replace(p, "")
-        s2 = s2.replace(p, "")
+    for symb in punct:
+        s1 = s1.replace(symb, "")
+        s2 = s2.replace(symb, "")
 
     words1 = set(s1.lower().split())
     words2 = set(s2.lower().split())
 
-    only_in_first = words1 - words2
-    only_in_second = words2 - words1
+    only_in_1 = words1 - words2
+    only_in_2 = words2 - words1
 
-    if not only_in_first and not only_in_second:
+    if not only_in_1 and not only_in_2:
         print("Все слова присутствуют в обеих строках")
     else:
-        if only_in_first:
-            print("Слова, которые есть в первой строке, но нет во второй:")
-            print(list(only_in_first))
+        if only_in_1:
+            print("Слова только в первой строке:")
+            print(*only_in_1)
 
-        if only_in_second:
-            print("Слова, которые есть во второй строке, но нет в первой:")
-            print(list(only_in_second))
+        if only_in_2:
+            print("Слова только во второй строке:")
+            print(*only_in_2)
+
+
+def main():
+    first = input("Введите первую строку: ")
+    second = input("Введите вторую строку: ")
+    compare_strings(first, second)
+
+
+if __name__ == "__main__":
+    main()
