5007a46ff68114a999fb80f79b314f3f9f16e202
Marat-Sirazetdinov <kakahavupvup20005@gmail.com>
2026-02-15 20:10:29 +0300

    Delete LAB01 directory

--- LAB01/task05.py
+++
@@ -1,7 +0,0 @@
-import random
-
-numbers = [random.randint(-10, 10) for i in range(20)]
-print("Исходный список", numbers)
-
-multiplies_of_3 = [num for num in range(10) if num % 3 == 0]
-print(multiplies_of_3)
