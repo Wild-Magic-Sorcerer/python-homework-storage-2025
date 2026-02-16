0edfa19d22f52139fddabf8bda73924e38218aaa
whydejection <lysenko.gv@edu.spbstu.ru>
2026-02-16 11:23:53 +0300

    Update lab01t4.py

--- Лысенко Глеб Владимирович/lab01/lab01t4.py
+++ Лысенко Глеб Владимирович/lab01/lab01t4.py
@@ -1,6 +1,8 @@
-from collections import Counter
 numbers = [1, 2, 3, 3, 1, 6, 5, 3, 7, 1]
-count_dict = Counter(numbers)
-for number, count in count_dict.items():
-    print(f"{number} — {count}")
 
+count_dict = {}
+for number in numbers:
+    count_dict[number] = count_dict.get(number, 0) + 1
+
+for number, count in count_dict.items():
+    print(f"{number} - {count}")
