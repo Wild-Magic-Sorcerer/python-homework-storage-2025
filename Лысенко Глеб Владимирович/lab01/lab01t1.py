882086312d484a83cf5844ade9af9ad02380be63
whydejection <lysenko.gv@edu.spbstu.ru>
2026-02-16 11:19:43 +0300

    Update lab01t1.py

--- Лысенко Глеб Владимирович/lab01/lab01t1.py
+++ Лысенко Глеб Владимирович/lab01/lab01t1.py
@@ -8,13 +8,22 @@ def main():
         except ValueError:
             print("Ошибка: Введите корректное целое число.")
 
-    min_number = min(numbers)
-    max_number = max(numbers)
+    min_number = numbers[0]
+    for num in numbers:
+        if num < min_number:
+            min_number = num
+
+    max_number = numbers[0]
+    for num in numbers:
+        if num > max_number:
+            max_number = num
+
     total_sum = sum(numbers)
 
     print(f"Минимальное число: {min_number}")
     print(f"Максимальное число: {max_number}")
     print(f"Сумма всех чисел: {total_sum}")
+
 
 if __name__ == "__main__":
     main()
